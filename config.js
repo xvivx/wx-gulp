58b8a1b80e823aa5619100b0aae7d40893acc18f
xvivx <915939795@qq.com>
2018-05-30 16:28:50 +0800

    添加自定义配置，读取失败按默认

--- config.js
+++ config.js
@@ -1,11 +1,22 @@
 const path = require('path');
-const appName = 'za-goda-xcx';
+
+let custom = null;
+
+try {
+    custom = require('./custom');
+} catch (e) {
+    console.log('未配置custom.js，按默认配置启动...');
+    custom = {};
+}
+
+const appName = custom.appName || 'app';
+const commonDir = custom.commonDir || '/common';
 const appRootPath = path.resolve(__dirname, appName);
 
 module.exports = {
     appName: appName,
     dirs: {
-        commonDir: '/common',
+        commonDir: commonDir,
         appRootDir: appRootPath,
         appJsonDir: path.resolve(appRootPath, 'app.json'),
         configJsonDir: path.resolve(appRootPath, 'project.config.json'),
